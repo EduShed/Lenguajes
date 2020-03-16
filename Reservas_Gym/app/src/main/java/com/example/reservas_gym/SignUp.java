78c803043b255f186506aaef568ce77322fffd86
EduShed <57167218+EduShed@users.noreply.github.com>
2020-03-15 22:38:56 -0500

    Reservas_Gym

--- Reservas_Gym/app/src/main/java/com/example/reservas_gym/SignUp.java
+++ Reservas_Gym/app/src/main/java/com/example/reservas_gym/SignUp.java
@@ -10,31 +10,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.VideoView;
 
-import com.jackandphantom.blurimage.BlurImage;
+import java.util.ArrayList;
 
-public class Login extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
     private VideoView udemBg;
     MediaPlayer player;
     int playerVidPos;
+
     Button btnSignUp;
+    EditText txtNom;
+    EditText txtApe;
+    EditText txtId;
+    EditText txtPass;
 
-
+    static final ArrayList<Estudiante> students = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         connect();
         setVideo();
-        launchSignUp();
+        signUp();
     }
 
     public void connect()
     {
-        udemBg = findViewById(R.id.video_bg);
-        btnSignUp = findViewById(R.id.btnNew_account);
+        udemBg = findViewById(R.id.video_bg2);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        txtNom = findViewById(R.id.txtNombre);
+        txtApe = findViewById(R.id.txtApellido);
+        txtId = findViewById(R.id.txtID);
+        txtPass = findViewById(R.id.txtPassword);
     }
     public void setVideo()
     {
@@ -56,21 +67,43 @@ public class Login extends AppCompatActivity {
     }
     public static Intent launcheME(Context ctx)
     {
-        return new Intent(ctx, Login.class);
+        return new Intent(ctx, SignUp.class);
     }
 
-    public void launchSignUp()
+    public void signUp()
     {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = SignUp.launcheME(Login.this);
-                startActivity(intent);
+                if(verifyId())
+                {
+                    Toast.makeText(getApplicationContext(), "El estudiante ya se encuentra en el sistema", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    students.add(new Estudiante(txtNom.getText().toString(), txtApe.getText().toString(), txtId.getText().toString(), txtPass.getText().toString()));
+                    Toast.makeText(getApplicationContext(), "Registro exitoso", Toast.LENGTH_LONG).show();
+                    Intent intent = Login.launcheME(SignUp.this);
+                    startActivity(intent);
+                }
+
             }
         });
-
     }
 
+    public boolean verifyId()
+    {
+        boolean found = false;
+        for (Estudiante s:students)
+        {
+            if(txtId.getText().toString().equals(s.getId()))
+            {
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
 
 
 
